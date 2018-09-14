50a68c7de3b23f70675b09e087c0642fa230a1f4
Hsyouhei <43191710+Hsyouhei@users.noreply.github.com>
2018-09-14 12:59:07 +0900

    list15を使い回転させた

--- list2/list2/list13.cpp
+++ list2/list2/list13.cpp
@@ -2,6 +2,14 @@
 #include<math.h>
 #define M_PI 3.14159265358979323846 
 
+/*リスト15*/
+static void timer(int dummy) {
+	glutTimerFunc(100, timer, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glRotated(3.0, 0.0, 0.0, 1.0);
+	glutPostRedisplay();
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_LINES);
@@ -35,6 +43,8 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_RGBA);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
+	/*リスト15呼び出し*/
+	glutTimerFunc(10, timer, 0);
 	glutReshapeFunc(resize);
 	init();
 	glutMainLoop();
